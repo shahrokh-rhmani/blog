5181f93db60650f122b69f8d715036229ff0a203
Ali <shahrokh.r1379@gmail.com>
2022-03-14 17:23:50 +0330

    make modles category

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status',)
     prepopulated_fields = {'slug':('title',)}
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,20 @@ class PublishManager(models.Manager):
     def get_queryset(self):
         return super(PublishManager, self).get_queryset().filter(status=True)
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
