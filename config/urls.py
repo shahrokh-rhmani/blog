496e0530cd397b7bf52a62795d48fd274b3717f4
Ali <shahrokh.r1379@gmail.com>
2022-03-24 19:52:07 +0430

    create file urls.py in account app & include config urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    path('account/', include('account.urls')),
 ]
 
 if settings.DEBUG:
