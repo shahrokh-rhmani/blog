907bea6ca476c27875468c56dee95e8dd572ad07
Ali <shahrokh.r1379@gmail.com>
2022-04-13 17:46:49 +0430

    add url change-password in this file

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,18 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password
+    path(
+         'password_change/',
+          auth_views.PasswordChangeView.as_view(),
+          name='password_change'
+    ),
+    path(
+        'password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done',
+    )
+
 
 ]
