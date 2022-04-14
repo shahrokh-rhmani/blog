816b0ab4c7fa644a36e75ef379f9be6644e8a754
Ali <shahrokh.r1379@gmail.com>
2022-04-14 19:26:06 +0430

    add url reset password

--- account/urls.py
+++ account/urls.py
@@ -14,7 +14,26 @@ urlpatterns = [
         'password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done',
-    )
-
-
+    ),
+    # reset password
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    paht(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
