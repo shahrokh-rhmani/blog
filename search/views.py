1ed3a3030a573db63b9a8164182ebccb2754b314
shahrokh <shahrokh.r1379@gmail.com>
2022-05-08 11:38:11 -0700

    create view search & create template

--- search/views.py
+++ search/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.views.generic import ListView
+from django.db.models import Q
 
-# Create your views here.
+
+
+
+class SearchResultView(ListView):
+    model = Post
+    context_object_name = 'search_list'
+    template_name = 'search/search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        object_list = Post.published.filter(
+            Q(title__icontains=query)| Q(description__icontains=query)
+        )
+        return object_list
