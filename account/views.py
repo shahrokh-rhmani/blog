a2578a985680471b1b967d3215598ef7bdd5b248
shahrokh <shahrokh.r1379@gmail.com>
2022-05-09 11:15:02 -0700

    create view register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+
+            context = {
+                'new_user': new_user
+            }
+            return render(request, 'account/register_done.html', context)
+    else:
+        user_form = UserRegistrationForm()
+    context = {
+        'user_form': user_form
+    }
+    return render(request, 'account/register.html', context)
