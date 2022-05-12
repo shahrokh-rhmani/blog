439a258030b7996a1be55c81de62830a6a262f42
shahrokh <shahrokh.r1379@gmail.com>
2022-05-12 08:35:04 -0700

    create class-view Profile

--- account/views.py
+++ account/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import UserRegistrationForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from .models import User
+from .forms import FormProfile
+from django.urls import reverse_lazy
 
 
 def register(request):
@@ -22,3 +26,20 @@ def register(request):
         'user_form': user_form
     }
     return render(request, 'account/register.html', context)
+
+
+class Profile(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = "registration/profile.html"
+    form_class = FormProfile
+    success_url = reverse_lazy('account:profile')
+
+    def get_object(self, queryset=None):
+        return User.objects.get(pk=self.request.user.pk)
+
+    def get_form_kwargs(self):
+        kwargs = super(Profile, self).get_form_kwargs()
+        kwargs.update({
+            'user': self.request.user
+        })
+        return kwargs
