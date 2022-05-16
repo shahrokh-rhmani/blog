894262c0b915bc16e088d84d67a57eb4aaa821dc
shahrokh <shahrokh.r1379@gmail.com>
2022-05-16 07:37:54 -0700

    add field is_author % special_user

--- account/models.py
+++ account/models.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 class User(AbstractUser):
-    pass
+    is_author = models.BooleanField(default=False)
+    special_user = models.DateTimeField(default=timezone.now)
+
+    def is_special_user(self):
+         if self.special_user > timezone.now():
+             return True
+         else:
+            return False
 
     def __str__(self):
         return self.username
