c7fc4544c46beaea6b830a16913a3abf954a74df
shahrokh <shahrokh.r1379@gmail.com>
2022-05-24 21:05:00 +0430

    add useradmin-fieldsets

--- account/admin.py
+++ account/admin.py
@@ -6,7 +6,22 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     add_form = UserRegistrationForm
+    list_display = UserAdmin.list_display + ('is_author', 'special_user',)
+    # fieldsets = UserAdmin.fieldsets
+    # fieldsets = ['is_author']
     # form = CustomUserChangeForm
     model = UserAdmin
+
+UserAdmin.fieldsets[2][1]['fields'] = (
+    'is_active',
+    'is_staff',
+    'is_superuser',
+    'is_author',
+    'special_user',
+    'groups',
+    'user_permissions',
+
+)
+
 
 admin.site.register(User, CustomUserAdmin)
